9caa79874965c4221ca47d1138fd9f82dcdb64fe
totarochristian <totaro.christian.18@gmail.com>
2023-04-12 15:23:41 +0200

    Code refactory: move up the template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<script setup>
-  import AppTitle from "./components/AppTitle.vue"
-</script>
-
 <template>
   <AppTitle></AppTitle>
 </template>
+
+<script setup>
+  import AppTitle from "./components/AppTitle.vue"
+</script>
 
 <style scoped>
 
