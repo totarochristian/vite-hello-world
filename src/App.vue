a59f9776ff875e6fd3f26e9a2ef8fb4841bb93a1
totarochristian <totaro.christian.18@gmail.com>
2023-04-12 15:41:47 +0200

    Move the AppTitle call inside the AppHero component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <AppHero></AppHero>
-  <AppTitle></AppTitle>
 </template>
 
 <script>
-  import AppTitle from "./components/AppTitle.vue";
   import AppHero from "./components/AppHero.vue";
 
   export default{
     name: 'App',
     components:{
-      AppTitle,
       AppHero
     },
     data(){
--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="hero">
+    <AppTitle></AppTitle>
     <img src="../assets/images/Super-hero-head-sm.jpg" alt="Hero">
   </div>
 </template>
 
 <script>
+  import AppTitle from "./AppTitle.vue";
+
   export default {
     name: "AppHero",
+    components:{
+      AppTitle
+    },
     data(){
       return{
         
