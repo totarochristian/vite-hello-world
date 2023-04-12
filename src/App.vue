d73c4908e74840f6601cf3e7f96da1a8abea065b
totarochristian <totaro.christian.18@gmail.com>
2023-04-12 15:32:11 +0200

    Add the AppHero in the App tempalte

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
+  <AppHero></AppHero>
   <AppTitle></AppTitle>
 </template>
 
 <script>
   import AppTitle from "./components/AppTitle.vue";
+  import AppHero from "./components/AppHero.vue";
 
   export default{
     name: 'App',
     components:{
-      AppTitle
+      AppTitle,
+      AppHero
     },
     data(){
       return {
